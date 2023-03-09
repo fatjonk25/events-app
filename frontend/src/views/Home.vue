a1e1ebd94bade50d2b426250914a747f7b11dea2
fatjonk25 <fk48566@ubt-uni.net>
2023-03-09 20:28:07 +0100

    added events-card-slider

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,21 +1,45 @@
 <template>
-  <div>
-     <img class="home-img" src="../assets/images/events-home.jpg" alt="events-home">
-     <div class="content">
-      <h3>This is Home Page</h3>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint illum delectus tempora iure inventore exercitationem libero repellendus, autem mollitia odit commodi? Consectetur doloribus at pariatur tenetur voluptas ex debitis quibusdam.</p>
-     </div>
-  </div>
+  <el-carousel :interval="4000" type="card" height="400px">
+    <el-carousel-item v-for="(item, index) in images" :key="index">
+      <img class="imageUrl" :src="item.imageUrl" :alt="item.altText" />
+      <h3 class="medium">{{ item.title }}</h3>
+      <p>{{ item.description }}</p>
+    </el-carousel-item>
+  </el-carousel>
 </template>
 
 <script>
 export default {
-  name: 'home-view'
-}
+  name: "home - view",
+  data() {
+    return {
+      images: [
+        {
+          imageUrl: "https://source.unsplash.com/random/800x800/?img=1",
+        },
+        {
+          imageUrl: "https://source.unsplash.com/random/800x800/?img=32",
+        },
+        {
+          imageUrl: "https://source.unsplash.com/random/800x800/?img=62",
+        },
+        {
+          imageUrl: "https://source.unsplash.com/random/800x800/?img=47",
+        },
+        {
+          imageUrl: "https://source.unsplash.com/random/800x800/?img=55",
+        },
+        {
+          imageUrl: "https://source.unsplash.com/random/800x800/?img=87",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-.home-img {
+/* .home-img {
   width: 100%;
   height: 400px;
   object-fit: cover;
@@ -27,5 +51,23 @@ export default {
   flex-direction: column;
   gap: 30px;
   text-align: center;
+} */
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+.imageUrl {
+  width: 600px;
 }
 </style>
