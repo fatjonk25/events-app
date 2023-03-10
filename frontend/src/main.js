6faaa79116ce9e4953ad84caa870ff3353b76a14
fatjonk25 <fk48566@ubt-uni.net>
2023-03-10 21:58:11 +0100

    About page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import "element-ui/lib/theme-chalk/index.css";
 import router from "./router";
 import store from "./store";
 import locale from "element-ui/lib/locale/lang/en";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.use(ElementUI, { locale });
 
--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -1,44 +1,69 @@
 <template>
   <div>
-      <div class="event-details">
-        <p>{{event.name}}</p>
-        <p>{{event.date}}</p>
-        <p>{{event.time}}</p>
-        <p>{{event.location}}</p>
-        <p>{{event.desc}}</p>
-      </div>
+    <div class="event-details">
+      <p>Event name: {{ event.name }}</p>
+      <p>Event date: {{ event.date }}</p>
+      <p>Event time: {{ event.time }}</p>
+      <p>Event location: {{ event.location }}</p>
+      <p>Event description: {{ event.desc }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name:'event-view',
-  props:['id'],
+  name: "event-view",
+  props: ["id"],
   data() {
     return {
-      event: {}
-    }
+      event: {},
+    };
   },
   methods: {
-    async getEvent(){
-      const response =  await fetch(`http://localhost:3000/events/${this.id}`);
+    async getEvent() {
+      const response = await fetch(`http://localhost:3000/events/${this.id}`);
 
       const data = await response.json();
 
       this.event = data;
-    }
+    },
   },
-  beforeMount(){
+  beforeMount() {
     this.getEvent();
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-
 .event-details {
-  width: 780px;
-  margin: 0 auto;
-  background-color: brown;
+  background-color: #f7f7f7;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  margin-top: 20px;
+}
+
+.event-details h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+.event-details p {
+  margin-bottom: 15px;
+}
+
+.event-details .date-time {
+  font-size: 16px;
+  color: #555;
+  margin-bottom: 5px;
+}
+
+.event-details .location {
+  font-size: 18px;
+  margin-bottom: 15px;
+}
+
+.event-details .description {
+  font-size: 16px;
+  line-height: 1.5;
 }
 </style>
