46878e943eb079d7893aef9fbb9db69c10796bc8
fatjonk25 <fk48566@ubt-uni.net>
2023-03-10 22:22:45 +0100

    Event details

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
     <div class="event-details">
-      <p>Event name: {{ event.name }}</p>
-      <p>Event date: {{ event.date }}</p>
-      <p>Event time: {{ event.time }}</p>
-      <p>Event location: {{ event.location }}</p>
-      <p>Event description: {{ event.desc }}</p>
+      <h2>{{ event.name }}</h2>
+      <div class="details-container">
+        <div class="details-row">
+          <i class="far fa-calendar-alt"></i>
+          <p>{{ event.date }}</p>
+        </div>
+        <div class="details-row">
+          <i class="far fa-clock"></i>
+          <p>{{ event.time }}</p>
+        </div>
+        <div class="details-row">
+          <i class="fas fa-map-marker-alt"></i>
+          <p>{{ event.location }}</p>
+        </div>
+      </div>
+      <p class="description">{{ event.desc }}</p>
     </div>
   </div>
 </template>
@@ -36,34 +47,41 @@ export default {
 
 <style scoped>
 .event-details {
-  background-color: #f7f7f7;
-  padding: 20px;
+  background-color: #fff;
+  padding: 30px;
   border-radius: 5px;
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
-  margin-top: 20px;
+  margin-top: 50px;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .event-details h2 {
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+.details-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+}
+
+.details-row {
+  display: flex;
+  align-items: center;
+  margin-right: 30px;
+}
+
+.details-row i {
   font-size: 24px;
-  margin-bottom: 10px;
-}
-.event-details p {
-  margin-bottom: 15px;
-}
-
-.event-details .date-time {
-  font-size: 16px;
+  margin-right: 10px;
   color: #555;
-  margin-bottom: 5px;
 }
 
-.event-details .location {
+.description {
   font-size: 18px;
-  margin-bottom: 15px;
-}
-
-.event-details .description {
-  font-size: 16px;
   line-height: 1.5;
 }
 </style>
