4e1c947902432173419607af8d1629ffd519183c
Gentuar Lushtaku <gl55390@ubt-uni.net>
2023-03-11 04:42:02 -0800

    fixed bootstrap error

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from "./router";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import store from "./store";
 import locale from "element-ui/lib/locale/lang/en";
-import "@fortawesome/fontawesome-free/css/all.css";
 import './assets/main.css'
 
 
